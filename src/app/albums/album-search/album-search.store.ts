97b8412a04d7979b13e76380ea907a79f8183bd7
Laslo Rajda <lra@enigmatry.com>
2024-05-23 15:41:59 +0200

    m-05

--- src/app/albums/album-search/album-search.store.ts
+++ src/app/albums/album-search/album-search.store.ts
@@ -5,21 +5,22 @@ import { computed, inject } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { AlbumsService } from "../albums.service";
 import { rxMethod } from "@ngrx/signals/rxjs-interop";
-import { exhaustMap, pipe, tap } from "rxjs";
+import { exhaustMap, filter, pipe, tap } from "rxjs";
 import { tapResponse } from "@ngrx/operators";
+import { setAllEntities, withEntities } from "@ngrx/signals/entities";
 import { setError, setPending, setSuccess, withRequestStatus } from "@/shared/state/route/request-status.feature";
 
 export const albumSearchStore = signalStore(
     withState({
-        albums: [] as Album[],
         query: '',
         order: 'asc' as SortOrder,
     }),
+    withEntities<Album>(),
     withRequestStatus(),
     withComputed(store => {
-        const filteredAlbums = computed(() => sortAlbums(searchAlbums(store.albums(), store.query()), store.order()));
+        const filteredAlbums = computed(() => sortAlbums(searchAlbums(store.entities(), store.query()), store.order()));
         return {
-            showSpinner: computed(() => store.isPending() && store.albums().length === 0),
+            showSpinner: computed(() => store.isPending() && store.entities().length === 0),
             filteredAlbums,
             totalAlbums: computed(() => filteredAlbums().length)
         }
@@ -32,20 +33,22 @@ export const albumSearchStore = signalStore(
                 tap(_ => patchState(store, setPending())),
                 exhaustMap(() => albumService.getAll().pipe(
                     tapResponse({
-                        next: albums => {
-                            patchState(store, setSuccess())
-                            patchState(store, { albums });
-                        },
+                        next: albums => patchState(store, setAllEntities(albums), setSuccess()),
                         error: (error: { message: string }) => {
-                            snackBar.open(error.message, 'Dismiss', { duration: 3000 });
                             patchState(store, setError(error.message));
                         }
                     })
-                ))))
+                )))),
+        notifyOnError: rxMethod<string | undefined>(
+            pipe(
+                filter(Boolean),
+                tap(message => snackBar.open(message, 'Dismiss', { duration: 5000 }))
+            ))
     })),
     withHooks({
-        onInit({ loadAllAlbums }) {
+        onInit({ loadAllAlbums, notifyOnError, error }) {
             loadAllAlbums();
+            notifyOnError(error);
         }
     })
 )
